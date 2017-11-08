9e386a9ed27f52b963e62ac280eb8843ff2f2373
amir <Blackworld.2035@gmail.com>
2017-11-08 17:41:11 +0330

    Observers added

--- app/src/main/java/net/teslaa/rxandroid_sample_app/MainActivity.java
+++ app/src/main/java/net/teslaa/rxandroid_sample_app/MainActivity.java
@@ -1,7 +1,20 @@
 package net.teslaa.rxandroid_sample_app;
 
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewTreeObserver;
+
+import org.reactivestreams.Subscription;
+
+import java.util.concurrent.Callable;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +22,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Observable<String> myObservable = Observable.just("Hello");
+
+        Observer<String> myObserver = new Observer<String>() {
+            @Override
+            public void onError(Throwable e) {
+                // Called when the observable encounters an error
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+                // Called each time the observable emits data
+                Log.d("MY OBSERVER", s);
+            }
+        };
+
+        //myObservable.subscribe(myObserver);
+
+    }
+
+    static Observable<String> sampleObservable() {
+        return Observable.defer(new Callable<ObservableSource<? extends String>>() {
+            @Override
+            public ObservableSource<? extends String> call() throws Exception {
+                // Do some long running operation
+                SystemClock.sleep(5000);
+                return Observable.just("one", "two", "three", "four", "five");
+            }
+        });
     }
 }
